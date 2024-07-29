93904aae028b7c7a19f3da4137f3b764a16e9858
Abdinajib123 <161884134+Abdinajib123@users.noreply.github.com>
2024-07-29 14:25:19 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
+import './App.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
