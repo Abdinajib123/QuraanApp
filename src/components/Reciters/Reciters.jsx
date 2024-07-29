0e62596c9db5545fa63854468a2a82063f72b253
Abdinajib123 <161884134+Abdinajib123@users.noreply.github.com>
2024-07-29 15:35:05 +0300

    Update Reciters.jsx

--- src/components/Reciters/Reciters.jsx
+++ src/components/Reciters/Reciters.jsx
@@ -6,7 +6,7 @@ const Reciters = ({ reciters, reciterHandler }) => {
   const [activeId, setActiveId] = useState('');
 
   return (
-    <div className='min-vh-100 shadow-lg p-8 pt-10 overflow-hidden'>
+    <div className='min-vh-100 shadow-lg p-3 pt-10 overflow-hidden'>
       <h1 className='fs-5 fw-bold text-center'>Reciters</h1>
       <hr />
       {reciters && reciters.length > 0 ? (
@@ -17,7 +17,7 @@ const Reciters = ({ reciters, reciterHandler }) => {
                 reciterHandler(reciter);
                 setActiveId(reciter.id);
               }}
-              className={`d-flex align-items-center py-0 cursor ${
+              className={`d-flex align-items-center p-8 py-0 cursor ${
                 reciter.id === activeId && 'active'
               }`}
             >
