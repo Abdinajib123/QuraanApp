e0a2b74fba08706d13638e6fdbf1d1f174d9746b
Abdinajib123 <161884134+Abdinajib123@users.noreply.github.com>
2024-07-29 22:19:58 +0300

    Update Players.jsx

--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -8,45 +8,45 @@ const Players = ({ reciterDetail, chapterDetail }) => {
 
   useEffect(() => {}, [chapterDetail, reciterDetail]);
 
-  const largeFontStyle = { fontSize: '1.5rem' };
+  const largeFontStyle = { fontSize: '1.25rem' };
 
   return (
     <div className='h-100 shadow-lg p-6 '>
-      <h1 className='fs-5 fw-bold text-center'>Player</h1>
+      <h1 className='fs-5 fw-bold text-center py-2'>Player</h1>
       <hr />
       {reciterDetail !== null && chapterDetail !== null ? (
         <ul className='list-group text-end'>
           <div>
             <li
-              className='list-group-item bg-transparent border-0 text-black py-0 d-flex justify-content-between'
+              className='list-group-item bg-transparent border-0 text-black py-2 d-flex justify-content-between'
             >
               <span className='fw-bold' style={largeFontStyle}>Reciter: </span>
               <span style={largeFontStyle}>{reciterDetail.name}</span>
             </li>
             <hr />
             <li
-              className='list-group-item bg-transparent border-0 text-black py-0 d-flex justify-content-between'
+              className='list-group-item bg-transparent border-0 text-black py-2 d-flex justify-content-between'
             >
               <span className='fw-bold' style={largeFontStyle}>Chapter In Arabic: </span>
               <span style={largeFontStyle}>{chapterDetail.name_arabic}</span>
             </li>
             <hr />
             <li
-              className='list-group-item bg-transparent border-0 text-black py-0 d-flex justify-content-between'
+              className='list-group-item bg-transparent border-0 text-black py-2 d-flex justify-content-between'
             >
               <span className='fw-bold' style={largeFontStyle}>Chapter In English: </span>
               <span style={largeFontStyle}>{chapterDetail.name_complex}</span>
             </li>
             <hr />
             <li
-              className='list-group-item bg-transparent border-0 text-black py-0 d-flex justify-content-between'
+              className='list-group-item bg-transparent border-0 text-black py-2 d-flex justify-content-between'
             >
               <span className='fw-bold' style={largeFontStyle}>Revelation Place: </span>
               <span style={largeFontStyle}>{chapterDetail.revelation_place}</span>
             </li>
             <hr />
             <li
-              className='list-group-item bg-transparent border-0 text-black py-0 d-flex justify-content-between'
+              className='list-group-item bg-transparent border-0 text-black py-2 d-flex justify-content-between'
             >
               <span className='fw-bold' style={largeFontStyle}>Translated Name: </span>
               <span style={largeFontStyle}>{chapterDetail.translated_name.name}</span>
